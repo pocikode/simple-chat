62b0dfcbaab9ecd6357d77050d19fb37ded891e5
Agus Supriyatna <aguzsupriyatna7@gmail.com>
2018-11-06 01:24:17 +0700

    change db config

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'forge'),
+            'host' => env('DB_HOST', 'ec2-23-21-192-179.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
