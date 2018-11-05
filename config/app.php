cd169390ccb8f666daa336b55bf390e9681d9bb4
Agus Supriyatna <aguzsupriyatna7@gmail.com>
2018-11-06 01:39:28 +0700

    change app key

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://simple-chat-3008.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
